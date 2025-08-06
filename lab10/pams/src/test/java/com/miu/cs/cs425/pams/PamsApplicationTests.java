4772df76f4a8e111e97284ab063387dfb89217ed
quynhdv <quynhvdinh@gmail.com>
2025-08-06 12:02:27 -0500

    lab10

---
+++ lab10/pams/src/test/java/com/miu/cs/cs425/pams/PamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.cs.cs425.pams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
