70dfba94ecb2422a0dc9a24de901c9ef4e7a66c6
quynhdv <quynhvdinh@gmail.com>
2025-08-06 15:13:04 -0500

    lombok

--- lab10/pams/src/main/java/com/miu/cs/cs425/pams/model/Patient.java
+++ lab10/pams/src/main/java/com/miu/cs/cs425/pams/model/Patient.java
@@ -2,6 +2,11 @@ package com.miu.cs.cs425.pams.model;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Patient {
     private String firstName;
     private String lastName;
@@ -9,63 +14,6 @@ public class Patient {
     private String email;
     private String mailingAddress;
     private LocalDate dateOfBirth;
-    // constructors, getters, and setters
-    public Patient(String firstName, String lastName, String contactPhoneNumber, String email, String mailingAddress, LocalDate dateOfBirth) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.contactPhoneNumber = contactPhoneNumber;
-        this.email = email;
-        this.mailingAddress = mailingAddress;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getContactPhoneNumber() {
-        return contactPhoneNumber;
-    }
-
-    public void setContactPhoneNumber(String contactPhoneNumber) {
-        this.contactPhoneNumber = contactPhoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMailingAddress() {
-        return mailingAddress;
-    }
-
-    public void setMailingAddress(String mailingAddress) {
-        this.mailingAddress = mailingAddress;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
 
     public String toJson() {
         return "{" +
