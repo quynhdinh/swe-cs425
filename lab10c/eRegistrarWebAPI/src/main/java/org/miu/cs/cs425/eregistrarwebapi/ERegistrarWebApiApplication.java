d17a835679751bebae585732085e32718d4d4681
quynhdv <quynhvdinh@gmail.com>
2025-08-08 14:03:26 -0500

    init 10c

---
+++ lab10c/eRegistrarWebAPI/src/main/java/org/miu/cs/cs425/eregistrarwebapi/ERegistrarWebApiApplication.java
@@ -0,0 +1,13 @@
+package org.miu.cs.cs425.eregistrarwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERegistrarWebApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERegistrarWebApiApplication.class, args);
+    }
+
+}
---
+++ lab10c/eRegistrarWebAPI/src/test/java/org/miu/cs/cs425/eregistrarwebapi/ERegistrarWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miu.cs.cs425.eregistrarwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegistrarWebApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
